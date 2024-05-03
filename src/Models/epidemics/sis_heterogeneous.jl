defd5a7beaa593d7caed520d610c38ab3bcab7ed
Federico Florio <fefiflo@live.it>
2024-05-03 15:24:06 +0200

    Rebased from source

--- src/Models/epidemics/sis_heterogeneous.jl
+++ src/Models/epidemics/sis_heterogeneous.jl
@@ -6,8 +6,9 @@ struct SIS_heterogeneous{T, N, F<:Real}
     ϕ  :: Vector{Vector{Vector{F}}}  # site observations
     ψ  :: Vector{Vector{Matrix{F}}}  # edge observations
     function SIS_heterogeneous(g::IndexedGraph, λ::SparseMatrixCSC{F,Int64}, ρ::Vector{F},
-            ϕ::Vector{Vector{Vector{F}}},
-            ψ::Vector{Vector{Matrix{F}}}) where {F<:Real}
+        α::Vector{F},
+        ϕ::Vector{Vector{Vector{F}}},
+        ψ::Vector{Vector{Matrix{F}}}) where {F<:Real}
         @assert size(λ)[1] == size(λ)[2] == nv(g)
         @assert length(ρ) == nv(g)
         @assert all(0 ≤ λᵢⱼ ≤ 1 for λᵢⱼ in !iszero(λ))
@@ -19,26 +20,27 @@ struct SIS_heterogeneous{T, N, F<:Real}
         @assert all(length(ϕᵢ) == T + 1 for ϕᵢ in ϕ)
         @assert length(ψ) == 2*ne(g)
         @assert all(length(ψᵢⱼ) == T + 1 for ψᵢⱼ in ψ)
-        new{T,N,F}(g, nonzeros(λ), ρ, α, ϕ, ψ)
+        new{T,N,F}(g, λ, ρ, α, ϕ, ψ)
     end
 end
 
 function SIS_heterogeneous(g::IndexedGraph{Int}, λ::SparseMatrixCSC{F,Int64}, ρ::Vector{F}, T::Int;
+        α = zeros(size(λ,1)),
         ψ = [[ones(2,2) for t in 0:T] for _ in 1:2*ne(g)],
         γ = 0.5,
         ϕ = [[t == 0 ? (length(γ)==1 ? [1-γ, γ] : [1-γ[i],γ[i]]) : ones(2) for t in 0:T] for i in vertices(g)]) where {F<:Real}
         
-    return SIS_heterogeneous(g, λ, ρ, ϕ, ψ)
+    return SIS_heterogeneous(g, λ, ρ, α, ϕ, ψ)
 end
 
-function SIS_heterogeneous(λ::SparseMatrixCSC{F,Int64}, ρ::Vector{F}, T::Int; γ=0.5) where {F<:Real}
+function SIS_heterogeneous(λ::SparseMatrixCSC{F,Int64}, ρ::Vector{F}, T::Int; γ=0.5, α::Vector{F}=zeros(size(λ,1))) where {F<:Real}
     A = ones(Int,size(λ)[1],size(λ)[2]) - iszero.(λ)
     g = IndexedGraph(A+A')
     
-    return SIS_heterogeneous(g, λ, ρ, T; γ=γ)
+    return SIS_heterogeneous(g, λ, ρ, T; γ, α)
 end
 
 # WARNING! The λs are all bound together
 function sis_heterogeneous_factors(sis::SIS_heterogeneous{T,N,F}) where {T,N,F}
-    [[SIS_heterogeneousFactor(nonzeros(sis.λ)[nzrange(sis.λ,i)], sis.ρ[i]) for _ in 1:T+1] for i in vertices(sis.g)]
+    [[SIS_heterogeneousFactor(nonzeros(sis.λ)[nzrange(sis.λ,i)], sis.ρ[i]; α=sis.α[i]) for _ in 1:T+1] for i in vertices(sis.g)]
 end
